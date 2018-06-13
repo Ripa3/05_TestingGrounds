869d0138c8cec9f0f6395ba988d52cacc6abeca6
DESKTOP-IJ42F00\bandi <bandieramontel@gmail.com>
2018-06-13 18:42:22 +0200

    AI in C++

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,8 +2,31 @@
 
 #include "ChooseNextWaypoint.h"
 #include "Public/ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "PatrolComponent.h"
+#include "AIController.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	//get patrol points
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolComponent = ControlledPawn->FindComponentByClass<UPatrolComponent>();
+
+	if (!ensure(PatrolComponent)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolComponent->GetPatrolPoints();
+
+	if (ensure(PatrolPoints.Num() == 0)) { return EBTNodeResult::Failed; }
+
+
+	//set nextwaypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//cycle index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
 	return EBTNodeResult::Succeeded;
 }
