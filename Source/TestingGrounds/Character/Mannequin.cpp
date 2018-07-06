da77030d332865c700d4cd7d1ef7c5ce8c3e64c2
DESKTOP-IJ42F00\bandi <bandieramontel@gmail.com>
2018-07-06 19:13:20 +0200

    Tiles are procedurally generated with randomized props

--- Source/TestingGrounds/Character/Mannequin.cpp
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -9,7 +9,7 @@
 AMannequin::AMannequin()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	//Create and setup the CameraComponent
 	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
---
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tile.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values
+ATile::ATile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSpawn)
+{
+	FVector Min = FVector(81.f, -1718.f, 80.f);
+	FVector Max = FVector(3837.f, 1984.f, 80.f);
+	FBox Bounds(Min, Max);
+	int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (size_t i = 0; i < NumberToSpawn; i++)
+	{
+		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
+		AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+		Spawned->SetActorRelativeLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
+	}
+
+}
+
+
+// Called when the game starts or when spawned
+void ATile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
